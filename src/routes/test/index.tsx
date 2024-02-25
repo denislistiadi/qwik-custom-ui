af42811bfb48cf3715df401e7a3b5fdfba8266c0
Denis Listiadi <denislistiadi24@gmail.com>
2024-02-25 12:28:47 +0700

    test(post): routeAction static

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik"
-import { routeLoader$ } from "@builder.io/qwik-city"
+import { Form, routeAction$, routeLoader$ } from "@builder.io/qwik-city"
 
 interface ItemProps {
   id?: number
@@ -14,10 +14,31 @@ export const useGetData = routeLoader$(async () => {
   return data
 })
 
+export const usePostData = routeAction$(async (data) => {
+  const resp = await fetch(
+    "https://65be164adcfcce42a6f1d0f8.mockapi.io/posts",
+    {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      // Send your data in the request body as JSON
+      body: JSON.stringify(data),
+    }
+  )
+  const obj = await resp.json()
+  return obj
+})
+
 export default component$(() => {
   const data = useGetData()
+  const action = usePostData()
   return (
     <div>
+      <Form action={action}>
+        <input name="title" placeholder="title" />
+        <input name="image" placeholder="image" />
+        <input name="desc" placeholder="desc" />
+        <button type="submit">Add user</button>
+      </Form>
       {data.value.map((item: ItemProps) => (
         <p key={item.id}>{item.title}</p>
       ))}
